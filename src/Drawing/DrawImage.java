3df1bc3ca8b0fb9f1d3858682eb246638cc692a2
frolovconst <frolovconst@gmail.com>
2017-01-28 10:03:53 +0300

    init

---
+++ src/Drawing/DrawImage.java
@@ -0,0 +1,16 @@
+package Drawing;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Carioca on 26/01/2017.
+ */
+public class DrawImage {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setSize(100,100);
+        window.setVisible(true);
+    }
+}
